98c9c1debd6f504b2b73276c48dfdfc315897aff
KrykukhaMaksymNure <maksym.krykukha@nure.ua>
2024-05-03 03:59:58 +0300

    init comit

---
+++ Task2/apz-pzpi-21-4-krykukha-maksym-task2/src/constants/enum.ts
@@ -0,0 +1,16 @@
+export enum WatcherRole{
+    ADMIN = 'ADMIN',
+    WATCHER = 'WATCHER'
+}
+
+export enum WatcherIsTaken{
+    FREE = 'FREE',
+    TAKEN = 'TAKEN'
+}
+
+export const cityTranslations = {
+    Kyiv: 'Київ',
+    Lviv: 'Львів',
+    Odesa: 'Одеса',
+    // Add more cities as needed
+  };
