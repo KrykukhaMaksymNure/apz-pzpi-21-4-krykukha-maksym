bcfc263b1186943ec5475dcd9f7a1bec206df545
KrykukhaMaksymNure <maksym.krykukha@nure.ua>
2024-05-10 18:33:37 +0300

    add configuration

---
+++ Task3/apz-pzpi-21-4-krykukha-maksym-task3/configuration.py
@@ -0,0 +1,10 @@
+import json
+
+def get_base_url():
+    try:
+        with open("configuration.json", "r") as f:
+            settings = json.load(f)
+            return settings["BASE_URL"]
+        
+    except Exception as e:
+        print("Invalid Request")
