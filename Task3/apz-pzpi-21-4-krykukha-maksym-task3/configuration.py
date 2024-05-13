7a0c60170feb4b7f416b1fa12751a02f290208db
KrykukhaMaksymNure <maksym.krykukha@nure.ua>
2024-05-14 01:09:41 +0300

    update configuration module

--- Task3/apz-pzpi-21-4-krykukha-maksym-task3/configuration.py
+++ Task3/apz-pzpi-21-4-krykukha-maksym-task3/configuration.py
@@ -5,6 +5,7 @@ def get_base_url():
         with open("configuration.json", "r") as f:
             settings = json.load(f)
             return settings["BASE_URL"]
-        
+
     except Exception as e:
         print("Invalid Request")
+
