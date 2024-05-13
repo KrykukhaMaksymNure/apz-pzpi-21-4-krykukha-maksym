b8694f93e41ce992e05c452942ee165eba275efb
KrykukhaMaksymNure <maksym.krykukha@nure.ua>
2024-05-14 01:10:18 +0300

    update authentication module

--- Task3/apz-pzpi-21-4-krykukha-maksym-task3/Auth.py
+++ Task3/apz-pzpi-21-4-krykukha-maksym-task3/Auth.py
@@ -7,51 +7,64 @@ BASE_URL = configuration.get_base_url()
 # Headers for localization
 headers = {"localization": "uk",}
 
-# Authentication data for watcher and driver
-auth_data_watcher = {    
-    "email":"test@gmail.com",    
-    "phone":"0123456789",    
-    "watcherName":"ADMIN",    
-    "watcherSurname":"ADMIN",    
-    "password":"ADMIN"
-}
-auth_data_driver = {    
-    "email":"test@gmail.com",    
-    "phone":"0123456789",    
-    "driverName":"DRIVER",    
-    "driverSurname":"DRIVER",    
-    "password":"DRIVER"
-}
-
-# Function to authenticate the watcher and return the response
-def authenticate_watcher() -> str:    
-    url = f"{BASE_URL}/auth/watcher_login"          
+# Function to authenticate the driver and return the response
+def authenticate_driver(iot) -> str:
+    url = f"{BASE_URL}/auth/driver_login"
     try:
-        # Send POST request for watcher authentication
-            response = requests.post(url, headers=headers, json=auth_data_watcher)    
+        email = input("Enter your email: ")
+        phone = input("Enter your phone: ")
+        driverName = input("Enter your name: ")
+        driverSurname = input("Enter your surname: ")
+        password = input("Enter your password: ")
+
+        # Authentication data for driver and driver
+        auth_data_driver = {}
+        auth_data_driver["email"] = email
+        auth_data_driver["phone"] = phone
+        auth_data_driver["driverName"] = driverName
+        auth_data_driver["driverSurname"] = driverSurname
+        auth_data_driver["password"] = password
+        # Send POST request for driver authentication
+        response = requests.post(url, headers=headers, json=auth_data_driver)
     except Exception as e :
         print("Error at authentication")
     return(response)
-    
 
-# Function to authenticate the driver and return the response
-def authenticate_driver(iot) -> str:    
-    url = f"{BASE_URL}/auth/driver_login"      
-    try:
-        # Send POST request for driver authentication
-            response = requests.post(url, headers=headers, json=auth_data_driver)        
-    except Exception as e :
-        print("Error at authentication")
-    return(response)    
 
-# Get watcher ID from authentication response
-def get_watcher_id():    
-    response = authenticate_watcher()    
-    print("Watcher", response.json()['watcher']['id'])    
-    return(response.json()['watcher']['id'])
 
 # Get driver ID from authentication response
-def get_driver_id(iot):   
-    response = authenticate_driver(iot)   
-    print("Driver", response.json()['driver']['id'])   
-    return(response.json()['driver']['id'])
+def get_driver_id(iot):
+    response = authenticate_driver(iot)
+
+    global driver_id
+    driver_id = (response.json()['driver']['id'])
+    print("Driver", driver_id)
+
+    return driver_id
+
+def get_not_taken_watchers():
+    url = f'{BASE_URL}/watcher/not-taken'
+
+    try:
+        response = requests.get(url)
+        return response.json()["id"]
+    except Exception as e:
+        print("Error occupied finding not taken Watcher")
+
+
+#Get watcher id after binding watcher to driver
+def get_watcher_id():
+    watcher_id = get_not_taken_watchers()
+
+    data_binding_watcher_to_driver = {
+        "watcherId" : watcher_id,
+        "driverId" : driver_id
+    }
+
+    url = f'{BASE_URL}/driver/add-watcher'
+
+    try:
+        response = requests.post(url,json=data_binding_watcher_to_driver)
+        return watcher_id
+    except Exception as e:
+        print("Error occupied at binding watcher")
