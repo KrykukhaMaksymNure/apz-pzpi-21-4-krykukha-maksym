6ca9582e783071be8ab7f5719307ca6d43de942b
KrykukhaMaksymNure <maksym.krykukha@nure.ua>
2024-05-14 01:09:23 +0300

    update session module

--- Task3/apz-pzpi-21-4-krykukha-maksym-task3/Session.py
+++ Task3/apz-pzpi-21-4-krykukha-maksym-task3/Session.py
@@ -1,33 +1,38 @@
-from datetime import datetime
 import requests
 import configuration
 
 BASE_URL = configuration.get_base_url()
 
 # Function to create a new session and return the session ID
-def create_session(session_name, driver_id,watcher_id):    
-    data = {        
-        "sessionName":session_name,        
-        "driverId":driver_id,        
-        "watcherId":watcher_id    
+def create_session(session_name, driver_id,watcher_id):
+    data = {
+        "sessionName":session_name,
+        "driverId":driver_id,
+        "watcherId":watcher_id
     }
-    url = f"{BASE_URL}/session/add"      
-    response = requests.post(url, json=data)        
+    url = f"{BASE_URL}/session/add"
+    response = requests.post(url, json=data)
     return(response.json()['id'])
 
 # Function to update session status (enable/disable)
-def update_session(status,session_id):    
-    data = {        
-        "isEnabled":status,    
+def update_session(status,session_id):
+    data = {
+        "isEnabled":status,
     }
-    url = f"{BASE_URL}/session/{session_id}"      
-    response = requests.patch(url, json=data)    
+    url = f"{BASE_URL}/session/{session_id}"
+    try:
+        response = requests.patch(url, json=data)
+    except Exception as e:
+        print("Error at updating session")
 
 # Function to check if a session is enabled
-def is_enabled_session(session):    
-    url = f"{BASE_URL}/session/{session}"      
-    response = requests.get(url)    
-    if (response.json()['isEnabled'] == "ENABLED"):        
-        return 'ENABLED'    
-    else:        
+def is_enabled_session(session):
+    url = f"{BASE_URL}/session/{session}"
+    response = requests.get(url)
+    if (response.json()['isEnabled'] == "ENABLED"):
+        return 'ENABLED'
+    else:
         return 'DISABLED'
+
+def end_session(session_id):
+    update_session('DISABLED', session_id)
